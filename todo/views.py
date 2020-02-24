6d0f25e5bb763f9dcb4fa1ae8645d0d0f4bd947d
zm235 <zm235@cornell.edu>
2020-02-24 11:15:29 -0500

    finish the overall view of the to do list

---
+++ todo/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import TodoItem
+
+# Create your views here.
+def todoView(response):
+    all_todo_items = TodoItem.objects.all()
+    return render(response, "todo.html", {'all_items' : all_todo_items})
+
+def addTodo(request):
+    new_item = TodoItem(content = request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+    # create a new todo all_items
+    # save
+    # redirect the browser to '/todo/'
